f12e8e1fad4ef1139a6c0ab60113aa9353ab522d
Muneeb Ahmed <68533379+MuneebAhmed568@users.noreply.github.com>
2023-08-28 12:14:36 +0500

    Update config.js

--- study/config.js
+++ study/config.js
@@ -1,7 +1,4 @@
 export default {
     hostname: 'https://localhost:5001/',
     json_Api: '/data.json'
-  //  json_Api: 'https://abroadstudy.pk/studyabroad/json_server/'
-        // hostname: '/studyabroad/api_server/',
-        // json_Api: '/studyabroad/json_server/'
 }
